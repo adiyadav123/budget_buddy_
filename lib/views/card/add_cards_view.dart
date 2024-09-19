df611d1dc676030c1c79c15077965e16fbb98292
Aditya Yadav <103302549+adiyadav123@users.noreply.github.com>
2024-09-20 00:51:46 +0530

    Add CardNumberFormatter for formatting card numbers

--- lib/views/card/add_cards_view.dart
+++ lib/views/card/add_cards_view.dart
@@ -3,6 +3,7 @@ import 'package:budgetbuddy/views/login/config.dart';
 import 'package:budgetbuddy/views/login/welcome.dart';
 import 'package:budgetbuddy/views/main_tab/main_tab_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:hive_flutter/adapters.dart';
 
@@ -350,5 +351,37 @@ class _AddCardsViewState extends State<AddCardsView> {
       Get.offAll(const MainTabView());
     }
     print("submitting card");
+  }
+}
+
+
+class CardNumberFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(
+    TextEditingValue previousValue,
+    TextEditingValue nextValue,
+  ) {
+    var inputText = nextValue.text;
+
+    if (nextValue.selection.baseOffset == 0) {
+      return nextValue;
+    }
+
+    var bufferString = StringBuffer();
+    for (int i = 0; i < inputText.length; i++) {
+      bufferString.write(inputText[i]);
+      var nonZeroIndexValue = i + 1;
+      if (nonZeroIndexValue % 4 == 0 && nonZeroIndexValue != inputText.length) {
+        bufferString.write(' ');
+      }
+    }
+
+    var string = bufferString.toString();
+    return nextValue.copyWith(
+      text: string,
+      selection: TextSelection.collapsed(
+        offset: string.length,
+      ),
+    );
   }
 }
